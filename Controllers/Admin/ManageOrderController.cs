ca90d17f4a2685585a3844895bcd4937f7c9a338
amisha-ambaliya <amishaambaliya12203@gmail.com>
2025-03-06 17:21:20 +0530

    amisha_ad

---
+++ Controllers/Admin/ManageOrderController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Foodie.Controllers.Admin
+{
+    public class ManageOrderController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
