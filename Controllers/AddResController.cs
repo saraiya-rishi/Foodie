59062209133756dcc4e7299de14d1b79ae97334e
saraiya-rishi <saraiyarishi8102@gmail.com>
2025-03-06 16:32:00 +0530

    Freash Start

---
+++ Controllers/AddResController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Foodie.Controllers
+{
+    public class AddResController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
